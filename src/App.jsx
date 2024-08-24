7c69215da879395ff86379c3ddf1295e73db0ade
Neel Doshi <neeldoshi147@gmail.com>
2024-08-24 23:44:57 +0530

    Join Now Button CSS

--- src/App.jsx
+++ src/App.jsx
@@ -8,21 +8,21 @@ function App() {
   return (
     <div>
       <Header />
-      <div className="container">
-
-        
+      <div style={{ height: "100vh" }} className="container">
         <h1 className='title-heading'>Lorem Ipsum is simply dummy text.</h1>
         <h2 className='subtitle-heading'>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</h2>
         <div className='container-wrapper'>
-        <div className='join-meeting'>
-          <label for="exampleInputEmail1" className='usernameLabel'>Email username</label>
-          <input type="email" className="form-control username" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"></input>
+          <div className='join-meeting'>
+            <label style={{color: "white"}} className='formLabels'> Room Id</label>
+            <input type="email" className="form-control formInputBS" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"></input>
 
-          <label for="exampleInputEmail1" className='usernameLabel'>Email username</label>
-          <input type="email" className="form-control username" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"></input>
+            <label style={{color: "white", marginTop : "16px"}} className='formLabels'>Username</label>
+            <input type="email" className="form-control formInputBS" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"></input>
+
+            <button style={{backgroundColor:"#68D585", border:"0"}}className='joinRoomBtn'>Join Room</button>
+            <label style={{color: "white"}}> Want to create Room? Let's create</label>
+          </div>
         </div>
-        </div>
-        
 
       </div>
     </div>
